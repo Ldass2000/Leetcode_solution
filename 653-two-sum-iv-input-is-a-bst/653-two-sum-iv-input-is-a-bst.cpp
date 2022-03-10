0497f38286bedb9dd93eda422e670d39b6996e74
Lalu Kumar <83582883+Ldass2000@users.noreply.github.com>
2022-03-10 14:05:20 +0530

    Time: 46 ms (73.65%), Space: 38.9 MB (31.29%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -10,33 +10,20 @@
  * };
  */
 class Solution {
+    
+    // USING HASHMAP
+    
+    private: 
+              unordered_set<int>s;
+    
 public:
     
-    void helper(TreeNode* root, vector<int>&ans)
-    {
-        if(root==NULL) return;
-        helper(root->left,ans);
-        ans.push_back(root->val);
-        helper(root->right,ans);
-    }
-    
-    bool findTarget(TreeNode* root, int k) {
-        // INORDER + TWO POINTER
-        //Do inorder traverse of BST  which give us back a sorted vector of all the tree's value.
-        //Then use two pointre to find  a Two Sum
-        //TC-->O(Recursive)
-        //SC-->O(N)
-        vector<int>ans;
-        helper(root,ans);
-        int i=0;
-        int j=ans.size()-1;
-         while(i<j)
-         {
-             int sum=ans[i]+ans[j];
-             if(sum==k) return 1;
-             if(sum>k) j--;
-             else  i++;
-         }
-        return 0;
+        bool findTarget(TreeNode* root, int k) {
+         if (!root) 
+            return false;
+        if (s.count(k - root->val)) 
+            return true;
+        s.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
     }
 };
