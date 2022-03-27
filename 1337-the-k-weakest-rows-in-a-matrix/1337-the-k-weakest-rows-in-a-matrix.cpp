acddd69890fcae0f53d0babc7e4a9224f5c7cad3
Lalu Kumar <83582883+Ldass2000@users.noreply.github.com>
2022-03-27 23:43:25 +0530

    Time: 18 ms (46.89%), Space: 10.7 MB (19.79%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        int rows=mat.size();
+        int cols=mat[0].size();
+        
+        vector<vector<int>>soldiers(max(rows,cols)+1);
+        vector<int>ans;
+        
+        for(int  row=0;row<rows;row++)
+        {
+            int totalSoldies=0;
+            
+            for(int col=0;col<cols;col++)
+            {
+                totalSoldies+=mat[row][col];
+            }
+            soldiers[totalSoldies].push_back(row);
+        }
+        
+        for(int r=0;r<soldiers.size();r++)
+        {
+            for(int c=0;c<soldiers[r].size();c++)
+            {
+                if(k>0)
+                {
+                    ans.push_back(soldiers[r][c]);
+                    k--;
+                    
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
