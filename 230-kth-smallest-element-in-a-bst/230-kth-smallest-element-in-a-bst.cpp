4ac11fd19e620f96bb0b155e2c701e10043df7a5
Lalu Kumar <83582883+Ldass2000@users.noreply.github.com>
2022-04-18 16:06:57 +0530

    Time: 15 ms (92.75%), Space: 24.1 MB (87.81%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -11,29 +11,45 @@
  */
 class Solution {
 public:
+    
+    void helper(TreeNode* root, int &k ,int &ans)
+    {
+        if(root==NULL) return;
+        
+        helper(root->left,k,ans);
+        k--;
+        if(k==0) ans=root->val;
+        
+        helper(root->right,k,ans);
+    }
+    
     int kthSmallest(TreeNode* root, int k) {
-        stack<TreeNode*>st;
-        int count=0;
-        while(true)
-        {
-            if(root!=NULL)
-            {
-                st.push(root);
-                root=root->left;
-            }
+//         stack<TreeNode*>st;
+//         int count=0;
+//         while(true)
+//         {
+//             if(root!=NULL)
+//             {
+//                 st.push(root);
+//                 root=root->left;
+//             }
             
-            else
-            {
-                if(st.empty())
-                    break;
-                root=st.top();
-                st.pop();
-                count++;
+//             else
+//             {
+//                 if(st.empty())
+//                     break;
+//                 root=st.top();
+//                 st.pop();
+//                 count++;
                 
-                if(count==k) return root->val;
-                root=root->right;
-            }
-        }
-        return -1;
+//                 if(count==k) return root->val;
+//                 root=root->right;
+//             }
+//         }
+//         return -1;
+        
+        int ans=0;
+        helper(root,k,ans);
+        return ans;
     }
 };
